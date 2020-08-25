e29210c68bbfceac15ea8e422c25848fd8a5afed
janewaitara <janewaitara99@gmail.com>
2020-08-25 11:22:13 +0300

    Added logic to setup the spinner

--- app/src/main/java/com/janewaitara/medec/authentication/RegisterActivity.kt
+++ app/src/main/java/com/janewaitara/medec/authentication/RegisterActivity.kt
@@ -2,6 +2,10 @@ package com.janewaitara.medec.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.janewaitara.medec.R
 import com.janewaitara.medec.common.extensions.isVisible
@@ -9,17 +13,38 @@ import com.janewaitara.medec.common.extensions.onClick
 import kotlinx.android.synthetic.main.activity_register.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private val registerViewModel: RegisterViewModel by viewModel()
+    private var arrayAdapter: ArrayAdapter<CharSequence>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        setupSpinner()
         setOnClickListener()
         subscribeToData()
     }
+
+    private fun setupSpinner() {
+       /* arrayAdapter = ArrayAdapter(applicationContext,android.R.layout.simple_spinner_dropdown_item, R.array.userRoles)*/
+
+        arrayAdapter = ArrayAdapter.createFromResource(this,R.array.userRoles, android.R.layout.simple_spinner_dropdown_item)
+       /* (arrayAdapter as ArrayAdapter<CharSequence>).setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+
+ */       register_UserRoles_spinner.adapter = arrayAdapter
+        register_UserRoles_spinner.onItemSelectedListener = this
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+       Toast.makeText(this,"Nothing selected",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+    }
+
 
     private fun setOnClickListener() {
         btn_register.onClick {
@@ -101,4 +126,5 @@ class RegisterActivity : AppCompatActivity() {
     private fun registerUser() {
         TODO("Not yet implemented")
     }
+
 }
